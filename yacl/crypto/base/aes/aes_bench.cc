8169b2342918a4cbfdcf38bced30a09f42e035d6
shanzhu.cjm <shanzhu.cjm@antgroup.com>
2023-09-15 14:14:54 +0800

    more fix

--- yacl/crypto/base/aes/aes_bench.cc
+++ yacl/crypto/base/aes/aes_bench.cc
@@ -83,37 +83,35 @@ static void BM_AesNiEcb(benchmark::State& state) {
 
 constexpr uint64_t kKeyWidth = 4;
 
-static void BM_MultiKeyAesEcb(benchmark::State& state) {
-  for (auto _ : state) {
-    state.PauseTiming();
-    std::array<uint128_t, kKeyWidth> keys_block;
-    std::array<uint128_t, kKeyWidth> plain_block;
+// FIXME: the follwoing causes CI build error, find out why
+// static void BM_MultiKeyAesEcb(benchmark::State& state) {
+//   for (auto _ : state) {
+//     state.PauseTiming();
+//     std::array<uint128_t, kKeyWidth> keys_block;
+//     std::array<uint128_t, kKeyWidth> plain_block;
 
-    std::random_device rd;
-    Prg<uint128_t> prg(rd());
+//     std::random_device rd;
+//     Prg<uint128_t> prg(rd());
 
-    uint128_t eb;
-    for (uint64_t i = 0; i < kKeyWidth; ++i) {
-      eb = prg();
-      keys_block[i] = eb;
-      eb = prg();
-      plain_block[i] = eb;
-    }
+//     for (uint64_t i = 0; i < kKeyWidth; ++i) {
+//       keys_block[i] = prg();
+//       plain_block[i] = prg();
+//     }
 
-    std::array<AES_KEY, kKeyWidth> aes_key;
-    for (size_t idx = 0; idx < kKeyWidth; ++idx) {
-      aes_key[idx] = AES_set_encrypt_key(idx);
-    }
+//     std::array<AES_KEY, kKeyWidth> aes_key;
+//     for (size_t idx = 0; idx < kKeyWidth; ++idx) {
+//       aes_key[idx] = AES_set_encrypt_key(idx);
+//     }
 
-    AES_opt_key_schedule<kKeyWidth>(keys_block.data(), aes_key.data());
-    size_t n = state.range(0);
-    state.ResumeTiming();
+//     AES_opt_key_schedule<kKeyWidth>(keys_block.data(), aes_key.data());
+//     size_t n = state.range(0);
+//     state.ResumeTiming();
 
-    for (size_t i = 0; i < n; i++) {
-      ParaEnc<kKeyWidth, 1>(plain_block.data(), aes_key.data());
-    }
-  }
-}
+//     for (size_t i = 0; i < n; i++) {
+//       ParaEnc<kKeyWidth, 1>(plain_block.data(), aes_key.data());
+//     }
+//   }
+// }
 
 #ifdef __x86_64
 static void BM_IppcpAes(benchmark::State& state) {
