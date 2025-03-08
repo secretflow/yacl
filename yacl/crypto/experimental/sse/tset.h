635541fc31ac6cab5bbf1ddc7943a90acd3603b1
Li Zhihang <1042740841@qq.com>
2025-03-08 15:21:28 +0800

    【OSCP】 + 在 YACL 上实现对称可搜索加密算法 (#423)
    * ss]
    
    * delete
    
    * move to yacl/examples
    
    * fix sse.h
    
    * fix problems
    
    * try to fix ci errors
    
    * try to fix UT
    
    * try to fix UT
    
    * fix problems
    
    * fix problems
    
    * fix problems
    
    * delete comments
    
    * delete Chinese
    
    * fixed
    
    * generate a longer K than bata_si
    
    * K length to achieve the security parameter λ 128
    
    * make it better
    
    * fix problems and add SNF
    
    * delete comments
    
    * fix ut
    
    * fix magic number and change the file directory
    
    * delete
    
    * clang-format and cpplint
    
    * README
    
    * camel
    
    * buildifier

---
+++ yacl/crypto/experimental/sse/tset.h
@@ -0,0 +1,75 @@
+// Copyright 2024 Li Zhihang.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#pragma once
+
+#include <set>
+#include <string>
+#include <unordered_map>
+#include <utility>
+#include <vector>
+
+#include "yacl/crypto/hash/ssl_hash.h"
+#include "yacl/crypto/hmac/hmac_sha256.h"
+#include "yacl/crypto/rand/rand.h"
+
+namespace yacl::crypto {
+
+class TSet {
+ public:
+  struct Record {
+    std::vector<uint8_t> label;
+    std::vector<uint8_t> value;
+  };
+
+  explicit TSet(int b, int s, int lambda);
+
+  std::string TSetSetup(
+      const std::unordered_map<
+          std::string,
+          std::vector<std::pair<std::vector<uint8_t>, std::string>>>& T,
+      const std::vector<std::string>& keywords);
+
+  // Get the tag for a given keyword
+  std::vector<uint8_t> TSetGetTag(const std::string& Kt,
+                                  const std::string& w) const;
+
+  // Retrieve records from the TSet
+  std::vector<std::pair<std::vector<uint8_t>, std::string>> TSetRetrieve(
+      const std::vector<std::vector<Record>>& TSet,
+      const std::string& stag) const;
+
+  const std::vector<std::vector<Record>>& GetTSet() const { return tset_; }
+  const std::vector<std::set<int>>& GetFree() const { return free_; }
+  static std::string VectorToString(const std::vector<uint8_t>& vec);
+
+ private:
+  void Initialize();
+  std::string Uint128ToString(__uint128_t value);
+
+  // Pack a pair of vector and string into a single vector
+  static std::vector<uint8_t> Pack(
+      const std::pair<std::vector<uint8_t>, std::string>& data);
+  // Unpack a vector into a pair of vector and string
+  static std::pair<std::vector<uint8_t>, std::string> UnPack(
+      const std::vector<uint8_t>& packed_data);
+
+  int b_;
+  int s_;
+  int lambda_;
+  std::vector<std::vector<Record>> tset_;
+  std::vector<std::set<int>> free_;
+};
+
+}  // namespace yacl::crypto
---
+++ yacl/crypto/experimental/sse/tset_test.cc
@@ -0,0 +1,97 @@
+// Copyright 2024 Li Zhihang.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include "yacl/crypto/experimental/sse/tset.h"
+
+#include <gtest/gtest.h>
+
+#include <memory>
+
+namespace yacl::crypto {
+
+class TSetTest : public ::testing::Test {
+ protected:
+  void SetUp() override {
+    tset_ = std::make_unique<TSet>(2,     // bucket_size (b)
+                                   3,     // slot_size (s)
+                                   128);  // lambda
+  }
+  std::unique_ptr<TSet> tset_;
+};
+
+TEST_F(TSetTest, Initialize) {
+  EXPECT_EQ(tset_->GetTSet().size(), 2);
+  EXPECT_EQ(tset_->GetTSet()[0].size(), 3);
+  EXPECT_EQ(tset_->GetTSet()[0][0].label.size(), 128 / 8);
+  EXPECT_EQ(tset_->GetTSet()[0][0].value.size(), 256 / 8 + 1);
+  EXPECT_EQ(tset_->GetFree().size(), 2);
+  EXPECT_EQ(tset_->GetFree()[0].size(), 3);
+}
+
+TEST_F(TSetTest, SetupAndGetTag) {
+  std::unordered_map<std::string,
+                     std::vector<std::pair<std::vector<uint8_t>, std::string>>>
+      T = {
+          {"keyword12",
+           {{{11, 12, 13, 14, 15, 16, 17, 18, 19}, "value1"},
+            {{21, 22, 23, 24, 25, 26, 27, 28, 29}, "value2"}}},
+          {"keyword34",
+           {{{31, 32, 33, 34, 35, 36, 37, 38, 39}, "value3"},
+            {{41, 42, 43, 44, 45, 46, 47, 48, 49}, "value4"}}},
+      };
+  std::vector<std::string> keywords = {"keyword12", "keyword34"};
+
+  auto Kt = tset_->TSetSetup(T, keywords);
+  auto TSet = tset_->GetTSet();
+  EXPECT_EQ(TSet.size(), 2);
+  EXPECT_EQ(TSet[0].size(), 3);
+  EXPECT_FALSE(Kt.empty());
+
+  auto tag = tset_->TSetGetTag(Kt, "keyword12");
+  EXPECT_EQ(tag.size(), 32);
+}
+
+// Test the complete retrieval flow
+TEST_F(TSetTest, CompleteRetrievalFlow) {
+  std::unordered_map<std::string,
+                     std::vector<std::pair<std::vector<uint8_t>, std::string>>>
+      T = {
+          {"keyword12",
+           {{{11, 12, 13, 14, 15, 16, 17, 18, 19}, "value1"},
+            {{21, 22, 23, 24, 25, 26, 27, 28, 29}, "value2"}}},
+          {"keyword34",
+           {{{31, 32, 33, 34, 35, 36, 37, 38, 39}, "value3"},
+            {{41, 42, 43, 44, 45, 46, 47, 48, 49}, "value4"}}},
+      };
+  std::vector<std::string> keywords = {"keyword12", "keyword34"};
+
+  auto Kt = tset_->TSetSetup(T, keywords);
+  auto TSet = tset_->GetTSet();
+
+  std::string w = "keyword12";
+  auto vector_stag = tset_->TSetGetTag(Kt, w);
+  std::string stag = tset_->VectorToString(vector_stag);
+
+  auto retrieved = tset_->TSetRetrieve(TSet, stag);
+
+  EXPECT_EQ(retrieved.size(), 2);
+  EXPECT_EQ(retrieved[0].first,
+            std::vector<uint8_t>({11, 12, 13, 14, 15, 16, 17, 18, 19}));
+  EXPECT_EQ(retrieved[0].second, "value1");
+  EXPECT_EQ(retrieved[1].first,
+            std::vector<uint8_t>({21, 22, 23, 24, 25, 26, 27, 28, 29}));
+  EXPECT_EQ(retrieved[1].second, "value2");
+}
+
+}  // namespace yacl::crypto
