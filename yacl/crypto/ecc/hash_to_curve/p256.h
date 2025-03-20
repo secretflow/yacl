54a8b57696078b050922dc9dc78adf1291c5a810
Ame-ReIori <reiori_ame@outlook.com>
2025-03-20 13:44:29 +0800

    add copyright, namespace and optimize function

--- yacl/crypto/ecc/hash_to_curve/p256.h
+++ yacl/crypto/ecc/hash_to_curve/p256.h
@@ -1,9 +1,27 @@
+// Copyright 2023 Ant Group Co., Ltd.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
 #pragma once
 
 #include <string>
 
 #include "yacl/base/byte_container_view.h"
 
+namespace yacl {
+
 // EncodeToCurve for P-256
 std::vector<uint8_t> EncodeToCurveP256(yacl::ByteContainerView buffer,
                                        const std::string &dst);
+
+}  // namespace yacl
