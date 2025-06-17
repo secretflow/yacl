fbbd17acf3582be259c33cdfee654fed4fe50957
Jamie <jamie.cui@outlook.com>
2025-06-17 11:17:25 +0800

    feat: add gc-based AES-128 and SHA-256 (#533)
    Co-authored-by: gitCoder1024 <201810111240@stu.shmtu.edu.cn>
    Co-authored-by: Rainysponge <375364412@qq.com>

---
+++ examples/gc/aes_run.cc
@@ -0,0 +1,92 @@
+// Copyright 2024 Ant Group Co., Ltd.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+#include <filesystem>
+#include <fstream>
+#include <future>
+#include <vector>
+
+#include "examples/gc/aes_128_evaluator.h"
+#include "examples/gc/aes_128_garbler.h"
+#include "fmt/format.h"
+
+#include "yacl/crypto/block_cipher/symmetric_crypto.h"
+
+namespace yacl {
+
+int aes_garbler_send_bytes = 0;
+int aes_evaluator_send_bytes = 0;
+int aes_compute_time = 0;
+
+void aes_performance() {
+  std::shared_ptr<yacl::io::BFCircuit> circ_;
+
+  GarblerAES* garbler = new GarblerAES();
+  EvaluatorAES* evaluator = new EvaluatorAES();
+
+  std::future<void> thread1 = std::async([&] { garbler->setup(); });
+  std::future<void> thread2 = std::async([&] { evaluator->setup(); });
+  thread1.get();
+  thread2.get();
+
+  std::string pth = fmt::format("yacl/io/circuit/data/{0}.txt", "aes_128");
+  yacl::io::CircuitReader reader(pth);
+  reader.ReadMeta();
+  reader.ReadAllGates();
+  circ_ = reader.StealCirc();
+
+  for (int i = 0; i < 1; i++) {
+    auto start1 = clock_start();
+    uint128_t key;
+    uint128_t message;
+    thread1 = std::async([&] { key = garbler->inputProcess(*circ_); });
+    thread2 = std::async([&] { message = evaluator->inputProcess(*circ_); });
+    thread1.get();
+    thread2.get();
+
+    // OT
+    thread1 = std::async([&] { evaluator->onLineOT(); });
+    thread2 = std::async([&] { garbler->onlineOT(); });
+    thread1.get();
+    thread2.get();
+
+    garbler->GB();
+    garbler->sendTable();
+
+    evaluator->recvTable();
+
+    evaluator->EV();
+
+    evaluator->sendOutput();
+
+    [[maybe_unused]] uint128_t gc_result = garbler->decode();
+    aes_compute_time += time_from(start1);
+    aes_garbler_send_bytes += garbler->send_bytes;
+    aes_evaluator_send_bytes += evaluator->send_bytes;
+  }
+  delete garbler;
+  delete evaluator;
+}
+}  // namespace yacl
+
+int main() {
+  yacl::aes_performance();
+  std::cout << "AES_performance:" << std::endl;
+  std::cout << "Garbler send: " << yacl::aes_garbler_send_bytes << " bytes"
+            << "  " << std::endl;
+  std::cout << "Evaluator send: " << yacl::aes_evaluator_send_bytes << " bytes"
+            << "  " << std::endl;
+  std::cout << "Time for Computing: " << yacl::aes_compute_time << "us"
+            << std::endl;
+}
---
+++ examples/gc/utils.h
@@ -0,0 +1,43 @@
+// Copyright 2024 Ant Group Co., Ltd.
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+#include <chrono>
+
+#include "yacl/base/byte_container_view.h"
+#include "yacl/base/int128.h"
+
+// get the Least Significant Bit of uint128_t
+inline bool getLSB(const uint128_t& x) { return (x & 1) == 1; }
+
+inline uint128_t ReverseBytes(uint128_t x) {
+  auto byte_view = yacl::ByteContainerView(&x, sizeof(x));
+  uint128_t ret = 0;
+  auto buf = std::vector<uint8_t>(sizeof(ret));
+  for (size_t i = 0; i < byte_view.size(); ++i) {
+    buf[byte_view.size() - i - 1] = byte_view[i];
+  }
+  std::memcpy(&ret, buf.data(), buf.size());
+  return ret;
+}
+
+inline std::chrono::time_point<std::chrono::high_resolution_clock>
+clock_start() {
+  return std::chrono::high_resolution_clock::now();
+}
+
+inline double time_from(
+    const std::chrono::time_point<std::chrono::high_resolution_clock>& s) {
+  return std::chrono::duration_cast<std::chrono::microseconds>(
+             std::chrono::high_resolution_clock::now() - s)
+      .count();
+}
