b3bc27bc143f9b8b48c5cfa8a793461f479ede62
changtong <36472950+changtong9@users.noreply.github.com>
2025-12-25 19:17:03 +0800

    update hesm2 (#619)

--- examples/hesm2/config.cc
+++ examples/hesm2/config.cc
@@ -37,8 +37,7 @@ void InitializeConfig() {
 
   // 检查是否成功创建
   if (!ec_group) {
-    std::cerr << "Failed to create SM2 curve using OpenSSL" << std::endl;
-    return;
+    YACL_THROW("Failed to create SM2 curve using OpenSSL");
   }
   // 检查文件是否存在，如果存在则从文件加载
   std::string filet1 = "cuckoo_t1.dat";
